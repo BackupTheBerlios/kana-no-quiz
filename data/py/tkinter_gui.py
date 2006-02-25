cda48e2850a6cfc9a1f42c5f8381e694602e4a3f
pachilor <devnull@localhost>
2006-02-25 20:50:27 +0000

    - "Quit" button implementation in Tkinter version completed.
     Modified Files:
     	data/py/gtk_gui.pyc data/py/i18n.pyc data/py/tkinter_gui.py
     	data/py/tkinter_gui.pyc

--- data/py/tkinter_gui.py
+++ data/py/tkinter_gui.py
@@ -74,8 +74,7 @@ class Gui:
 		button.pack(fill="both",expand=1)
 		button = tk.Button(frame2,text=str(4),command=self.about)
 		button.pack(fill="both",expand=1)
-		#~ button = tk.Button(frame2,text=str(71),command=self.quit)
-		button = tk.Button(frame2,text=str(71))
+		button = tk.Button(frame2,text=str(71),command=self.quit)
 		button.pack(fill="both",expand=1)
 
 		self.window.mainloop()
@@ -624,3 +623,6 @@ class Gui:
 	def setKanaImage(self,kind,kana):
 		"""Update kana image."""
 		self.image["file"] = os.path.join(self.datarootpath,"img/kana/%s_%s.gif" % (("k","h")[kind],kana))
+
+	def quit(self):
+		self.window.destroy() #Bye~~
